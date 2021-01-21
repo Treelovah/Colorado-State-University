e72259ce89ab0db5070844beddab82e08bc40ecd
Marshall Livingston <mltechi@pm.me>
2021-01-21 00:04:36 -0700

    Setting up first week of courses

---
+++ ACT-205/calculator.py
@@ -0,0 +1,11 @@
+# Assets = Liabilities + Stockholders' Equity
+# Stockholders' Equity = Assets - Liabilities
+
+# common stock = 
+#   Total Equity = Preferred Stock - Additional Paid-in Capital Retained
+#   Earngings + Treasury Stock
+#
+# retained earnings = 
+#   (net income + previous retained earnings) - net dividends paid to
+#   shareholders
+
